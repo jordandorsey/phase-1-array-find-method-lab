a0e534342842abbd77aef86d01462216748cf663
JordanDorsey <jordandorsey04@gmail.com>
2023-10-11 19:27:34 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(arrayOfObjects) {
+ const fun = arrayOfObjects.find((object) => object.result === "W" )
+ console.log(fun)
+    if (fun === undefined) {
+        return undefined
+    } else {
+        return fun.year
+    }
+}
